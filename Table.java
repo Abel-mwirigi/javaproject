485f20a47308cc82e384b670232837a7ac37a572
Getonto3 <getontomoses18@gmail.com>
2023-12-03 16:18:54 +0300

    minor changes on the Parent class and the main

--- Table.java
+++ Table.java
@@ -7,7 +7,7 @@ public abstract class Table {
     private double firstDrawer=2500;
     private double additionalDrawer=2000;
     private double discountRate=0.05;
-    private int drawers;
+    private int drawers, drawerCost;
 
     public abstract double WoodPrice();
     public Table(String typeOfWood,double length,double width,int drawers)
@@ -18,22 +18,29 @@ public abstract class Table {
     }   
     public double totalCost() {
     double customSize=((width*length-9600)*aboveStandardSize);
-    int drawerCost=(int) (firstDrawer+Math.max(0, drawers-1)*additionalDrawer);
+    drawerCost=(int) (firstDrawer+Math.max(0, drawers-1)*additionalDrawer);
 
     return basePrice+customSize+WoodPrice()+drawerCost;
     }
 
     public void displayDetails(int numberOfTables){
-      double  discount=(numberOfTables>1)?totalCost()*discountRate:0;
+      double  discount=(numberOfTables>=2)?totalCost()*discountRate:0;
       double total=totalCost()-discount;
 
-        System.out.println("Wood Type: " + typeOfWood);
-        System.out.println("Dimensions: " + length +"cm "+ width+ " cm");
-        System.out.println("Wood price: Ksh " + WoodPrice());  
-        System.out.println("Table Cost: Ksh " + totalCost());
-        System.out.println("Discount: Ksh" + discount);
-        System.out.println("Drawers: " + drawers);
-        System.out.println("Total Cost: Ksh " + total);
+       System.out.println("============================= ");
+
+        System.out.println("Wood Type   : " + typeOfWood);
+        System.out.println("Dimensions  : " + length +" cm "+ width+ " cm");
+        System.out.println("Drawers     : " + drawers);
+        System.out.println("No of Tables: " + numberOfTables);
+        System.out.println("Drawer cost : Ksh" + drawerCost);
+        System.out.println("Wood price  : Ksh " + WoodPrice());  
+        System.out.println("Table Cost  : Ksh " + totalCost());
+        System.out.println("Discount    : Ksh" + discount);
+        System.out.println("Total Cost  : Ksh " + total);
+
+         System.out.println("============================= ");
+
 
     }
 
--- TablesMain.java
+++ TablesMain.java
@@ -19,41 +19,61 @@ public class TablesMain {
 
         System.out.print("Welcome To Antique Furniture Kenya Limited Westlands: \n");
         System.out.print(" We specialize in making custom wooden tables as per your specifications\n");
-          System.out.print(
-                "A standard table whose area is 9600cm square (80 cm wide and 120cm long) is Kshs 42500 \n ");
-            System.out.println(
-                    "What dimensions of the table would you like? NOTE , each cm square above the standard size is charged at Kshs 825.");
-            System.out.print("Enter table length in cm: ");
-            length = myInput.nextDouble();
-            System.out.print("Enter table width in cm: ");
-            width = myInput.nextDouble();
-      
+        System.out.println("A standard table whose area is 9600cm square (80 cm wide and 120cm long) is Kshs 42500 \n Would you like the standard size or custom size? (STANDARD/CUSTOM)");
+        String choice = myInput.nextLine();
 
+        switch (choice.toLowerCase()) {
+            case "standard":
+                length=120;
+                width=80;
+                break;
+            case "custom":
+        System.out.println("What dimensions of the table would you like? NOTE , each cm square above the standard size is charged at Kshs 825.");
+        System.out.print("Enter table length in cm (should be more than 120cm): ");
+            length = myInput.nextDouble();
+        System.out.print("Enter table width in cm(should be more than 80cm): ");
+            width = myInput.nextDouble();
+                break;
+            default:
+                System.out.println("CHOOSE STANDARD OR CUSTOM SIZE");
+                return;
+        }
+                  
+        do 
+        { 
         System.out.println("How many drawers would you like added to your table? NOTE The first drawer goes for Ksh 2500 while additional drawers go for Kshs 2000.");
         System.out.println("Enter number of drawers (0-6): ");
         drawers = myInput.nextInt();
+            
+         if (drawers >6) {
+            System.out.println("Invalid input. Please enter 0-6");
+         }
+
+        } while (drawers>6);
+
         System.out.println(" What type of  wood would you like (Mahogany, Oak, Pine): Mahogany has an additional charge of Ksh 3000, for Oak the extra cost will be Ksh 2015. \nThere is no extra charge for using Pine.");
         String woodType = myInput.next().toLowerCase();
 
-        Table table;
+        Table newtable;
         switch (woodType.toLowerCase()) {
             case "mahogany":
-                table = new Mahogany("",width, length, drawers);
+                newtable = new Mahogany("Mahogany",width, length, drawers);
                 break;
             case "oak":
-                table = new Oak("",width, length, drawers);
+                newtable = new Oak("Oak",width, length, drawers);
                 break;
             case "pine":
-                table = new Pine("",width, length, drawers);
+               newtable = new Pine("Pine",width, length, drawers);
                 break;
             default:
-                System.out.println("Invalid wood type. Exiting program.");
+                System.out.println("Invalid wood type.");
                 return;
         }
 
-        System.out.print("Enter the number of tables: ");
+        System.out.println("How many tables of the same specification would you like? ");
         int numberOfTables = myInput.nextInt();
-        table.displayDetails(numberOfTables);
+
+        newtable.displayDetails(numberOfTables);
     }
 
 }
